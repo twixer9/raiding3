cfcb1a94343285aa4bd8c23e38c2e24c97872379
twixer9 <23smahendran@student.dasd.org>
2022-11-19 20:18:01 -0500

    COMPLETED BRONZE-2

--- bronze/src/main/java/BronzeRaids.java
+++ bronze/src/main/java/BronzeRaids.java
@@ -1,2 +1,3 @@
-public class BronzeRaids {
+
+public class BronzeRaids {// i did bronze-2; the code for this is in the triangle and triangleRunner class
 }
--- onyx/src/main/java/OnyxRaids.java
+++ onyx/src/main/java/OnyxRaids.java
@@ -1,2 +1,24 @@
+/*BEGINNING OF CODE FOR ONYX-1
+import java.util.Scanner;
 public class OnyxRaids {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String name = sc.nextLine();
+        System.out.println("Please enter your strength (numerical value)");
+        int strength = sc.nextInt();
+        System.out.println("Please enter your health (numerical value)");
+        int health = sc.nextInt();
+        Object weapon = new String ();
+        while (weapon.equals("")){
+            System.out.println("Please choose your weapon: STAFF OR WAND");
+             weapon = sc.nextLine();
+        }
+
+        Player person = new Player(name, strength, health, weapon);
+        Weapon user = new Weapon(weapon);
+        System.out.println(person);
+
+    }
 }
+//END OF CODE FOR ONYX-1
